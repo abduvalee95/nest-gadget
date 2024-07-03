17d7a2f7d4515fddcdf2c4b7e31f9bf8182c312f
abduvalee95 <abduvalee95@gmail.com>
2024-07-03 22:49:53 +0900

    fix: instalation Packages & modify format prettier

--- apps/gadget-batch/src/gadget-batch.service.ts
+++ apps/gadget-batch/src/gadget-batch.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class GadgetBatchService {
-  getHello(): string {
-    return 'Welcome To NEST- GADGET BATCH ';
-  }
+	getHello(): string {
+		return 'Welcome To NEST- GADGET BATCH ';
+	}
 }
