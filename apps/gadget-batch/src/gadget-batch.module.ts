edbe24ad380e4e6bec0b6d52048e4650b7e982c1
abduvalee95 <abduvalee95@gmail.com>
2024-07-03 22:42:34 +0900

    feat: Configure .env variables Ports Api:3007 and batch:3008

--- apps/gadget-batch/src/gadget-batch.module.ts
+++ apps/gadget-batch/src/gadget-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GadgetBatchController } from './gadget-batch.controller';
 import { GadgetBatchService } from './gadget-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [GadgetBatchController],
   providers: [GadgetBatchService],
 })
