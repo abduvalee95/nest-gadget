8f313f18d405aac10a33ce18ef774c57c82d5d09
abduvalee95 <abduvalee95@gmail.com>
2024-07-04 19:15:40 +0900

    fix: Connect MongoDB DataBase

--- apps/nest-gadget/src/database/database.module.ts
+++ apps/nest-gadget/src/database/database.module.ts
@@ -1,5 +1,26 @@
-
 import { Module } from '@nestjs/common';
+import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
-@Module({})
-export class DatabaseModule {}
+@Module({
+	imports: [
+		MongooseModule.forRootAsync({
+			useFactory: () => ({
+				uri: process.env.NODE_ENV === 'production' ? process.env.MONGO_PROD : process.env.MONGO_DEV,
+			}),
+		}),
+	],
+
+	exports: [MongooseModule],
+})
+export class DatabaseModule {
+	constructor(@InjectConnection() private readonly connection: Connection) {
+		if (connection.readyState === 1) {
+			console.log(
+				`MongoDB is Connected Into ${process.env.NODE_ENV === 'production' ? 'Production' : 'Development'} DATABASE`,
+			);
+		} else {
+			console.log('DATABASE Connection is FAILED !!!');
+		}
+	}
+}
