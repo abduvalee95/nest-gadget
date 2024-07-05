1214bb710bd8b18cca76ec9755018a183e3399c2
abduvalee95 <abduvalee95@gmail.com>
2024-07-05 21:58:40 +0900

    feat: Create Component related Modules

--- apps/nest-gadget/src/components/components.module.ts
+++ apps/nest-gadget/src/components/components.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
-import {MemberModule} from './member/member.module'
-import {GadgetModule} from './gadget/gadget.module'
+import { AuthModule } from './auth/auth.module';
+import { BoardArticleModule } from './board-article/board-article.module';
+import { CommentModule } from './comment/comment.module';
+import { FollowModule } from './follow/follow.module';
+import { GadgetModule } from './gadget/gadget.module';
+import { LikeModule } from './like/like.module';
+import { MemberModule } from './member/member.module';
+import { ViewModule } from './view/view.module';
 
 @Module({
-    imports:[MemberModule, GadgetModule]
+	imports: [
+		MemberModule,
+		GadgetModule,
+		AuthModule,
+		BoardArticleModule,
+		CommentModule,
+		FollowModule,
+		LikeModule,
+		ViewModule,
+	],
 })
-
-export class ComponentsModule{}
+export class ComponentsModule {}
