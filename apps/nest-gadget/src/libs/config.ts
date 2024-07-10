ba7b844cc4d1abc141005363178bb16f6ce75634
abduvalee95 <abduvalee95@gmail.com>
2024-07-10 21:03:27 +0900

    feat: develop Single and MultiImages Uploader Mutation GraphQL api

--- apps/nest-gadget/src/libs/config.ts
+++ apps/nest-gadget/src/libs/config.ts
@@ -1,8 +1,17 @@
 import { ObjectId } from 'bson';
-
+import * as path from 'path';
+import { v4 as uuidv4 } from 'uuid';
 
 export const shapeIntoMongoObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
+};
+
+//* IMAGE CONFIGURATION
+
+export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const getSerialForImage = (filename: string) => {
+	const ext = path.parse(filename).ext;
+	return uuidv4() + ext;
 };
 
 // Agenti sort qilish tartibi
--- apps/nest-gadget/src/main.ts
+++ apps/nest-gadget/src/main.ts
@@ -2,11 +2,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
+import { graphqlUploadExpress } from 'graphql-upload';
+import * as express from 'express';
+
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
 	app.useGlobalInterceptors(new LoggingInterceptor());
+
+	app.enableCors({ origin: true, credentials: true });
+	app.use(graphqlUploadExpress({ maxFileSize: 11500000, maxFile: 10 })); //** miqdori va nechta yuklash kkligni yozdik */
+	app.use('/uploads', express.static('./uploads')); //**Tashqi olamga static folder Sifatida ochib berdik */
 	await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
