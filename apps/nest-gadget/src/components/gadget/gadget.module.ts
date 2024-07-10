b5169926dcebd5b9dc6c541ae0c5365bfafb7562
abduvalee95 <abduvalee95@gmail.com>
2024-07-10 21:35:57 +0900

    feat: create gadget components

--- apps/nest-gadget/src/components/gadget/gadget.module.ts
+++ apps/nest-gadget/src/components/gadget/gadget.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import GadgetSchema from '../../schemas/Gadget.model';
+import { GadgetResolver } from './gadget.resolver';
+import { GadgetService } from './gadget.service';
+import { AuthModule } from '../auth/auth.module'
+import { ViewModule } from '../view/view.module'
 
-@Module({})
+@Module({
+	imports: [
+		MongooseModule.forFeature([
+			{
+				name: 'Gadget',
+				schema: GadgetSchema,
+			},
+		]),
+    AuthModule,
+    ViewModule,
+	],
+	providers: [GadgetService, GadgetResolver],
+})
 export class GadgetModule {}
